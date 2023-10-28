b923ba61f08d4b0e735cebe653bd280dc49364a4
unknown <nektoto289@gmail.com>
2023-10-28 09:04:51 +0300

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.swiper', {
+    loop:true,
+    navigation: {
+      nextEl: '.slider-main-block__arrow.swiper-button-next',
+      prevEl: '.slider-main-block__arrow.swiper-button-prev',
+    },
+  });
