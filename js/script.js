b3c18083cc9bc0b3dcc95f471b0140b4c9998353
unknown <nektoto289@gmail.com>
2023-10-28 11:32:51 +0300

    main block

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper', {
     loop:true,
     navigation: {
-      nextEl: '.slider-main-block__arrow.swiper-button-next',
-      prevEl: '.slider-main-block__arrow.swiper-button-prev',
+      nextEl: '.main-block__arrow.swiper-button-next',
+      prevEl: '.main-block__arrow.swiper-button-prev',
     },
   });
