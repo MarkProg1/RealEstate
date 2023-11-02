56b9d8ec8bca936f0d263084e5d1c286a28caefd
unknown <nektoto289@gmail.com>
2023-11-02 16:22:38 +0200

    add deals and featured block

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// slider
 const swiper = new Swiper('.swiper', {
     loop:true,
     navigation: {
@@ -5,3 +6,35 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.main-block__arrow.swiper-button-prev',
     },
   });
+
+
+
+// tabs
+
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click",tabSwitcher);
+
+
+function tabSwitcher(event){
+
+  let currentActiveTabIndex = null;
+  let nextActiveTabIndex = null;
+  
+  const targetElement = event.target;
+  if( targetElement.closest(".tabs-deals__button")){
+    tabNavItems.forEach((tabNavItem,index)=>{
+      if(tabNavItem.classList.contains('active'))
+      {
+        currentActiveTabIndex = index;
+        tabNavItem.classList.remove('active');
+      }
+      if(tabNavItem === targetElement){
+        nextActiveTabIndex = index;
+      }
+      })
+  }
+  targetElement.classList.add('active');
+  tabItems[currentActiveTabIndex].classList.remove('active');
+  tabItems[nextActiveTabIndex].classList.add('active');
+}
